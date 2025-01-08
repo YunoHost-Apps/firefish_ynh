cebcfd44e0ef7d6657c25a0ace766b56803dde58
Félix Piédallu <felix@piedallu.me>
2025-01-08 22:19:15 +0100

    revert legacy changes

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,13 +17,11 @@ NODEJS_VERSION="22"
 build_firefish() {
     pushd "$install_dir"
 
-        ynh_replace_string --match_string='"re2": "1.20.8",' --replace_string='"re2": "1.20.12",' --target_file="packages/backend/package.json"
-
         curl https://sh.rustup.rs -sSf | ynh_exec_warn_less ynh_exec_as $app sh -s -- -y
         export PATH="$install_dir/.cargo/bin:$PATH"
         ynh_use_nodejs
         env $ynh_node_load_PATH corepack enable
-        env $ynh_node_load_PATH COREPACK_ENABLE_DOWNLOAD_PROMPT=0 corepack prepare pnpm@8.15.5 --activate
+        env $ynh_node_load_PATH COREPACK_ENABLE_DOWNLOAD_PROMPT=0 corepack prepare pnpm@latest-9 --activate
 
         # Update all dependencies to the latest compatible versions
         ynh_exec_warn_less ynh_exec_as $app env $ynh_node_load_PATH ncu -u
